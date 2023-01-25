3705b134407dffdb6b6eb08d719ef11f81fd8065
jay-borsaniya <jayborsaniya.softrefine@gmail.com>
2023-01-25 15:32:43 +0530

    first commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from pymongo import MongoClient
 from .extensions import jwt, ma, mail
-from .. import configuration 
 
 MONGO_URI = 'mongodb+srv://jay:Jay123@cluster0.nvj7by8.mongodb.net/?retryWrites=true&w=majority'
 MONGO_DB = 'exercise'
@@ -22,16 +21,16 @@ def create_app():
 def register_extensions(app):
     ma.init_app(app)
     # app.config.from_pyfile('../configuration.cfg')
-    app.config['SECRET_KEY'] = configuration.SECRET_KEY
-    app.config['MAIL_SERVER'] = configuration.MAIL_SERVER
-    app.config['MAIL_PORT'] = configuration.MAIL_PORT
-    app.config['MAIL_USE_TLS'] = configuration.MAIL_USE_TLS
-    app.config['MAIL_USE_SSL'] = configuration.MAIL_USE_SSL
-    app.config['MAIL_USERNAME'] = configuration.MAIL_USERNAME
-    app.config['MAIL_PASSWORD'] = configuration.MAIL_PASSWORD
-    app.config['MAIL_DEFAULT_SENDER'] = configuration.MAIL_DEFAULT_SENDER
-    app.config['MAIL_MAX_EMAILS'] = configuration.MAIL_MAX_EMAILS
-    app.config['MAIL_ASCII_ATTACHMENTS'] = configuration.MAIL_ASCII_ATTACHMENTS
+    app.config['SECRET_KEY'] = "secret"
+    app.config['MAIL_SERVER'] = "smtp.gmail.com"
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USE_SSL'] = False
+    app.config['MAIL_USERNAME'] = "jayborsaniya.softrefine@gmail.com"
+    app.config['MAIL_PASSWORD'] = "tjawfxxarwteltuf"
+    app.config['MAIL_DEFAULT_SENDER'] = ("jay", "jayborsaniya.softrefine@gmail.com")
+    app.config['MAIL_MAX_EMAILS'] = None
+    app.config['MAIL_ASCII_ATTACHMENTS'] = False
 
     jwt.init_app(app)
     mail.init_app(app)
